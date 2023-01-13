4e8a74c80167652599987b4168d4df5cfe86a2d8
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2023-01-13 11:30:54 +0100

    ajout des fichiers principaux

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.EasyContract.app',
+  appName: 'EasyContract',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
